05c15d5666490baed2bababa87aad2de1f667b46
Fabian <fabians1999@gmail.com>
2020-02-02 16:13:26 +0100

    Added Clean for preventing ghosting

--- desk_ink.py
+++ desk_ink.py
@@ -4,7 +4,7 @@ import configparser
 import argparse
 import logging
 import time
-import datetime 
+import datetime
 
 from PIL import Image, ImageDraw, ImageFont
 from pyowm import OWM
@@ -14,15 +14,14 @@ from mail_handler import MailHandler
 from airhorn import Airhorn
 from lib import epd4in2
 
-
 FONT24 = ImageFont.truetype('Font.ttc', 24)
 FONT18 = ImageFont.truetype('Font.ttc', 18)
 MONO18 = ImageFont.truetype('Monospace.ttf', 18)
 FONT35 = ImageFont.truetype('Font.ttc', 35)
 
+
 class DeskInk:
     """Docstring for DeskInk. """
-
     def __init__(self, config: configparser.ConfigParser):
         self.logger = logging.getLogger("DeskInk")
         self.config = config
@@ -32,7 +31,8 @@ class DeskInk:
         self.logger.info("Init OWM...")
         self.owm = OWM(self.config["owm"]["key"])
         self.logger.info("Init Airhorn...")
-        self.airhorn = Airhorn(self.config["airhorn"]["pid"], self.config["airhorn"]["tempid"])
+        self.airhorn = Airhorn(self.config["airhorn"]["pid"],
+                               self.config["airhorn"]["tempid"])
         self.logger.info("Init MailHandler...")
         self.mail = MailHandler(config)
 
@@ -54,13 +54,14 @@ class DeskInk:
             mail = self.mail.count_mails()
             self.logger.info("  Draw and display...")
             image = self.draw(weather, airhorn, mail)
+            self.epd.Clear()
             self.epd.display(self.epd.getbuffer(image))
             self.logger.info("  Now sleeping for 600 seconds")
             time.sleep(600)
 
     def draw(self, weather, airhorn, mail):
-
         dt = datetime.datetime.now()
+
         icon_name = weather[0]
         icon = Image.open("icons/{}.jpg".format(icon_name))
 
@@ -79,34 +80,40 @@ class DeskInk:
 
         temp_text = "{} °C".format(airhorn["temperature"])
         temp_size = draw.textsize(temp_text, FONT35)
-        draw.text((100 - temp_size[0]/2 , 120), temp_text, font = FONT35, fill = 0)
+        draw.text((100 - temp_size[0] / 2, 120),
+                  temp_text,
+                  font=FONT35,
+                  fill=0)
 
-        tmm_text = "{} - {} °C".format(tempminmax["temp_min"], tempminmax["temp_max"])
+        tmm_text = "{} - {} °C".format(tempminmax["temp_min"],
+                                       tempminmax["temp_max"])
         tmm_size = draw.textsize(tmm_text, FONT18)
-        draw.text((100- tmm_size[0]/2, 160), tmm_text, font = FONT18, fill = 0)
+        draw.text((100 - tmm_size[0] / 2, 160), tmm_text, font=FONT18, fill=0)
 
         p_text = "{} und {} µg/m³".format(airhorn["p2.5"], airhorn["p10"])
         p_size = draw.textsize(p_text, FONT18)
-        draw.text((100 - p_size[0]/2, 185), p_text, font = FONT18, fill = 0)
+        draw.text((100 - p_size[0] / 2, 185), p_text, font=FONT18, fill=0)
 
         Himage.paste(mail_icon, (275, 15))
         if mail != 0:
-            draw.text((315, 7), str(mail), font = FONT18, fill = 0)
-        
-        draw.rectangle((0,275, 400, 300), fill=0)
-        draw.text((10, 276), os.uname().nodename , font = MONO18, fill = 255)
-        
+            draw.text((315, 7), str(mail), font=FONT18, fill=0)
+
+        draw.rectangle((0, 275, 400, 300), fill=0)
+        draw.text((10, 276), os.uname().nodename, font=MONO18, fill=255)
+
         time_text = dt.strftime("%Y-%m-%d %H:%M:%S")
-        time_size = draw.textsize(time_text, font= MONO18)
-        draw.text((390-time_size[0], 276), time_text, font = MONO18, fill = 255)
+        time_size = draw.textsize(time_text, font=MONO18)
+        draw.text((390 - time_size[0], 276), time_text, font=MONO18, fill=255)
 
         return Himage
-        
+
+
 def start(args):
     config = crypto_config.get_config(args.key.encode(), args.config)
 
     desk = DeskInk(config)
     desk.run()
+
 
 def main():
     parser = argparse.ArgumentParser("DeskInk")
@@ -120,23 +127,24 @@ def main():
     start_parser.add_argument("-c", "--config", default="config")
     start_parser.add_argument("-k", "--key", required=True)
 
-
     arguments = parser.parse_args()
 
     if arguments.subcommand == "edit":
         crypto_config.edit_config(arguments.key.encode(), arguments.config)
     elif arguments.subcommand == "start":
-        start(arguments) 
+        start(arguments)
 
 
 if __name__ == "__main__":
     try:
-        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         main()
     except IOError as e:
         logging.info(e)
-        
-    except KeyboardInterrupt:    
+
+    except KeyboardInterrupt:
         logging.info("ctrl + c:")
         epd4in2.epdconfig.module_exit()
         exit()
